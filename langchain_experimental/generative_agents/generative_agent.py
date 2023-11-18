7d1ff61c2a34cb35e189d41736278f884715d431
ramzd18 <rbpeddu@gmail.com>
2023-11-18 17:13:39 -0500

    Increased prompt insihgt generation

--- langchain_experimental/generative_agents/generative_agent.py
+++ langchain_experimental/generative_agents/generative_agent.py
@@ -344,7 +344,7 @@ Relevant context:
             "Here is a summary of {name}: {summary}"
             "Here are {name}'s interests: {interests} \n"
              "{name}'s current status: {status} \n"
-            "Given this generate a list of insights {name} would have based on reading these articles. Write the insights from the perspective of {name} and only include {name}'s personal insights and how they relate to their information and current situation. Make sure they are personalized insights. Seperate the insights with a semicolon."
+            "Given this generate a list of insights {name} would have based on reading these articles. Write the insights from the perspective of {name} and only include {name}'s personal insights and how they relate to their information and current situation. Make sure they are personalized insights. Write as many insights as you can. Seperate the insights with a semicolon."
             "Here is an example format  insight1; insight2;insight3;insight4;insight5;insight6 and so on"
         )
             soc_mem=self.memory.fetch_socialmedia_memories("interests")
