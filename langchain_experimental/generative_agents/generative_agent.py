acfcad22aff3fa920b164f0c6ecc32ad69f9f252
ramzd18 <rbpeddu@gmail.com>
2024-01-03 14:30:03 -0500

    context

--- langchain_experimental/generative_agents/generative_agent.py
+++ langchain_experimental/generative_agents/generative_agent.py
@@ -194,19 +194,20 @@ Context from memory:
         return result
     def  search_description_questions(self, description,  last_k: int = 25) -> List[str]:
         prompt = PromptTemplate.from_template(
+            "You are being tasked with generating questiosn a person would search online."
             "Summary of {name}: "
             "{summary}"
             " Here ia a description of the person that was inputted: {description}"
             "This description likely details a problem or general persona a person fits under."
-            "Given this relevant information from a persons memories, what are twelve relevant things you think they would search up to fit the desciprtion that was inputted. \n"
-            "Tailor the questions so you are covering what products this persons may use to solve their problems/ fit their persona.For example if the person inputted description of a person struggling to mantain personal finances, you might look up different budgeting products online, their pros/and cons, the basics of maintaining budget, and other relevant things to help you be more knowledagble about the topic."
+            "Given this relevant information about the person from their summary, what are twelve relevant things you think they would search up to fit the desciprtion that was inputted. \n"
+            "Tailor the questions so you are covering what products this persons may use to solve their problems/fit their persona.For example if the person inputted description of a person struggling to mantain personal finances, you might look up different budgeting products online, their pros/and cons, the basics of maintaining budget, and other relevant things to help you be more knowledagble about the topic."
             " Make sure the questions relate to {description}."
-            "MAKE SURE YOU GENERATE 12 QUESTIONS."
-            "Seperate each question with ;."
+            "MAKE SURE YOU GENERATE 12 QUESTIONS. DO NOT RETURN LESS QUESTIONS. MAKE 12. SEPERATE EACH ONE OF THE QUESTIONS WITH A ; when you return them."
         )
         summary=self.get_summary()
         result = self.chain(prompt).run(name=self.name,summary=summary,description=description)
         result= result.split(";")
+        print(result)
         return result
 
     # def generate_reaction(
